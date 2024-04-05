e8c6e05cf160cb1814556b7a31ff2314df55f0f9
f4faysal <f4faysals@gmail.com>
2024-04-06 00:00:49 +0600

    add a row view

--- App.js
+++ App.js
@@ -1,4 +1,43 @@
-import { ImageBackground, StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+const RowView = ({ label, value }) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 8,
+      }}
+    >
+      <View>
+        <Text
+          style={{
+            fontFamily: "Inter-regular",
+            color: "#303030",
+            fontSize: 10,
+            letterSpacing: 2,
+            textTransform: "uppercase",
+          }}
+        >
+          {label}
+        </Text>
+      </View>
+
+      <View>
+        <Text
+          style={{
+            fontFamily: "Inter-bold",
+            fontSize: 20,
+            color: "#303030",
+          }}
+        >
+          {value}
+        </Text>
+      </View>
+    </View>
+  );
+};
 
 export default function App() {
   return (
