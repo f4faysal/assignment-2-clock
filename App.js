de736c6006836b766da2d332889c82f4c55c7ed7
f4faysal <f4faysals@gmail.com>
2024-04-05 23:56:25 +0600

    add image

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      style={styles.container}
+      source={require("./assets/bacground.png")}
+    >
+      <Text style={{
+        fontSize : 22,
+        color : "white"
+      }}>
+      hihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihi
+      </Text>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
